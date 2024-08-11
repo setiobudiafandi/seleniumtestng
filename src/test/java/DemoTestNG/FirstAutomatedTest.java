b6968144c4439a08a1c74aa8f672bbcf0b7bd524
setiobudiafandi <setiobudi71@gmail.com>
2024-08-11 16:24:50 +0700

    up to adding priority for test to run

--- src/test/java/DemoTestNG/FirstAutomatedTest.java
+++ src/test/java/DemoTestNG/FirstAutomatedTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,7 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class FirstAutomatedTest {
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -20,14 +22,20 @@ public class FirstAutomatedTest {
         driver.get("https://www.lambdatest.com/selenium-playground");
     }
 
-    @AfterClass
+    @AfterMethod
     public void teardown() {
-        // driver.quit();
+        driver.quit();
     }
 
-    @Test
+    @Test(priority = 1)
     public void testTableSortAndSearch() {
         driver.findElement(By.linkText("Table Sort & Search")).click();
-        driver.findElement(By.xpath("//div[@id='example_filter']//input[@type='search']")).sendKeys("A. Ramos");
+        driver.findElement(By.xpath("//div[@id='example_filter']//input[@type='search']")).sendKeys("C. Hurst");
+    }
+
+    @Test(priority = 2)
+    public void testBootstrapDatePicker() {
+        driver.findElement(By.linkText("Bootstrap Date Picker")).click();
+        driver.findElement(By.id("birthday")).sendKeys("08-30-1995");
     }
 }
